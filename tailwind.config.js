790acf318d4b6aef67019a6d65697c53b8a63d17
brendanmclaughlin <bmclaughlin0407@gmail.com>
2023-12-05 12:46:14 -0800

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      'bolt': '#DDF329',
+      'boots': '#57574B',
+      'bootslight': '#6C6D5D',
+      'bootslight40': 'rgba(108,109,93,.2)',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
